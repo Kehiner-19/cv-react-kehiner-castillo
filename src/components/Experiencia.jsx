51567b12661bd94842e03095947467aad7da7dfe
Kehiner-19 <kehiner.cv71@gmail.com>
2025-06-04 21:09:32 +0000

    feat: componente Experiencia con 4 cargos definidos

---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,25 @@
+export default function Experiencia() {
+  return (
+    <section>
+      <h3>Experiencia Profesional</h3>
+      <ul>
+        <li>
+          <strong>Desarrollador Full Stack</strong> - Navisoft Gaming (2022-2024)<br />
+          Desarrollo de plataformas web para videojuegos, integración de APIs y mantenimiento de bases de datos.
+        </li>
+        <li>
+          <strong>Instructor TIC</strong> - SENA (2020-2022)<br />
+          Formación en programación básica y herramientas TIC a jóvenes y adultos en programas técnicos.
+        </li>
+        <li>
+          <strong>Freelancer Front-End</strong> - Autónomo (2019-2020)<br />
+          Creación de sitios web responsivos para pequeños negocios usando HTML, CSS y JavaScript.
+        </li>
+        <li>
+          <strong>Soporte Técnico</strong> - TechNet Colombia (2017-2019)<br />
+          Atención al cliente, solución de incidencias técnicas y soporte en instalación de software.
+        </li>
+      </ul>
+    </section>
+  );
+}
