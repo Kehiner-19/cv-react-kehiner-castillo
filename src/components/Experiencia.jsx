430095d2586e4c14d660fc2bfd9688f70e1fb37b
Kehiner-19 <kehiner.cv71@gmail.com>
2025-06-09 19:25:30 +0000

    feat: componente Experiencia dinamico con 10 cargosdefinido

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,7 +1,12 @@
 export default function Educacion() {
-  const educacion = [
-    { id: 1, titulo: "Ingeniería en Sistemas", universidad: "Universidad Nacional", periodo: "2018-2022" }
-  ];
+  const educacion = [  
+  { id: 1, titulo: "Ingeniería en Sistemas", universidad: "Universidad Nacional", periodo: "2018–2022" },
+  { id: 2, titulo: "Técnico en Programación", universidad: "SENA", periodo: "2016–2017" },
+  { id: 3, titulo: "Curso de React.js", universidad: "Platzi", periodo: "2023" },
+  { id: 4, titulo: "Taller de Diseño UX/UI", universidad: "Google Actívate", periodo: "2022" },
+  { id: 5, titulo: "Seminario de Ciberseguridad", universidad: "Fundación Telefónica", periodo: "2021" },
+];
+
 
   return (
     <section>
